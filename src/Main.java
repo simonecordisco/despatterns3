351b9bfdd2e21894b342b0effd6c40ce88a35baf
simonecordisco <147503322+simonecordisco@users.noreply.github.com>
2023-11-28 12:03:24 +0100

    Update Main.java
    uppdate

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,12 @@
 
 
 public class Main {
-    private static User db;
-    public static User getDb(){
-        if(db == null){
-            db = new User("Mario",26);
-            System.out.println(db);
-        }
-        return db;
-    }
+  
     public static void main(String[] args) {
-        User user2 = new User("Franco",45);
+              User user2 =  User.getDb();
         user2.informazioniAlVideo();
         user2.setName("Luigi");
         user2.setAge(33);
         user2.informazioniAlVideo();
-
-     User localDB = getDb();
     }
 }
